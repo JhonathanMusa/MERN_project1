71491277016b152018c9c46595fd1404ee426ff0
jhonathanmusa <jhonathansamu91@gmail.com>
2021-01-30 14:36:08 -0500

    adding post method

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
 import { MONGOURI } from "./keys";
-import path from "path";
-import morgan from "morgan";
 import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// page encode
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Database connection
 mongoose.connect(MONGOURI, { useUnifiedTopology: true, useNewUrlParser: true });
-mongoose.connection.on("connected", (err) => {
+
+const connection = mongoose.connection;
+
+connection.on("Connected", (err) => {
   if (err) {
     resizeBy.json(err);
   }
   console.log("Connected to MongoDB");
 });
-
-app.use(cors());
-app.use(morgan("tiny"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require("./routes/routes"));
 
